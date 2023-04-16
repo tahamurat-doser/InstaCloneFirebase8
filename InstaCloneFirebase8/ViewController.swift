0e34a94cd3fddfbc9f866e0d030399332b672cad
taha murat <ttahamuratdoser@gmail.com>
2023-04-16 22:05:05 +0300

    Initial Commit

---
+++ InstaCloneFirebase8/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  InstaCloneFirebase8
+//
+//  Created by Taha  on 16.04.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
